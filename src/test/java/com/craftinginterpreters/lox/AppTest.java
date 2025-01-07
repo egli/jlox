cb7d9296fa010286115937a00201fa9ed2cdf6cf
Christian Egli <christian.egli@sbs.ch>
2025-01-07 11:09:23 +0100

    Add a mini test

---
+++ src/test/java/com/craftinginterpreters/lox/AppTest.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.lox;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
