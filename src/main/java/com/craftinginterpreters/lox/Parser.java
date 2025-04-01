7de1f49b2b38f30527765436525646c732919c9c
Christian Egli <christian.egli@sbs.ch>
2025-04-01 11:20:04 +0200

    Implement variables, assignement and scope

--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -1,5 +1,6 @@
 package com.craftinginterpreters.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
@@ -13,16 +14,90 @@ class Parser {
 	this.tokens = tokens;
     }
 
-    Expr parse() {
-	try {
-	    return expression();
-	} catch (ParseError error) {
-	    return null;
+    List<Stmt> parse() {
+	List<Stmt> statements = new ArrayList<>();
+	while (!isAtEnd()) {
+	    statements.add(declaration());
 	}
+
+	return statements;
+    }
+
+    private Stmt statement() {
+	if (match(PRINT)) return printStatement();
+	if (match(LEFT_BRACE)) return new Stmt.Block(block());
+
+	return expressionStatement();
+    }
+
+    private Stmt printStatement() {
+	Expr value = expression();
+
+	consume(SEMICOLON, "Expect ';' after value.");
+	return new Stmt.Print(value);
+    }
+
+    private Stmt varDeclaration() {
+	Token name = consume(IDENTIFIER, "Expect variable name.");
+
+	Expr initializer = null;
+	if (match(EQUAL)) {
+	    initializer = expression();
+	}
+
+	consume(SEMICOLON, "Expect ';' after variable declaration.");
+	return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt expressionStatement() {
+	Expr value = expression();
+
+	consume(SEMICOLON, "Expect ';' after value.");
+	return new Stmt.Expression(value);
+    }
+
+    private List<Stmt> block() {
+	List<Stmt> statements = new ArrayList<>();
+
+	while (!check(RIGHT_BRACE) && !isAtEnd()) {
+	    statements.add(declaration());
+	}
+
+	consume(RIGHT_BRACE, "Expect '}' after block.");
+	return statements;
+    }
+
+    private Expr assignement() {
+	Expr expr = equality();
+
+	if (match(EQUAL)) {
+	    Token equals = previous();
+	    Expr value = assignement();
+
+	    if (expr instanceof Expr.Variable) {
+		Token name = ((Expr.Variable)expr).name;
+		return new Expr.Assign(name, value);
+	    }
+
+	    error(equals, "Invalid assignement target.");
+	}
+
+	return expr;
     }
 
     private Expr expression() {
-	return equality();
+	return assignement();
+    }
+
+    private Stmt declaration() {
+	try {
+	    if (match(VAR)) return varDeclaration();
+
+	    return statement();
+	} catch (ParseError error) {
+	    synchronize();
+	    return null;
+	}
     }
 
     private Expr equality() {
@@ -87,6 +162,9 @@ class Parser {
 	    return new Expr.Literal(previous().literal);
 	}
 
+	if (match(IDENTIFIER)) {
+	    return new Expr.Variable(previous());
+	}
 	if (match(LEFT_PAREN)) {
 	    Expr expr = expression();
 	    consume(RIGHT_PAREN, "Expect ')' after expression.");
--- src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -16,10 +16,19 @@ public class GenerateAst {
 
 	defineAst(outputDir,
 		  "Expr",
-		  Arrays.asList("Binary   : Expr left, Token operator, Expr right",
+		  Arrays.asList("Assign   : Token name, Expr value",
+				"Binary   : Expr left, Token operator, Expr right",
 				"Grouping : Expr expression",
 				"Literal  : Object value",
-				"Unary    : Token operator, Expr right"));
+				"Unary    : Token operator, Expr right",
+				"Variable : Token name"));
+
+	defineAst(outputDir,
+		  "Stmt",
+		  Arrays.asList("Block      : List<Stmt> statements",
+				"Expression : Expr expression",
+				"Print      : Expr expression",
+				"Var        : Token name, Expr initializer"));
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types)
