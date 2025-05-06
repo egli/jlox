0407d872085b79713d730733e66126aaa53376bf
Christian Egli <christian.egli@sbs.ch>
2025-05-06 11:24:25 +0200

    Handle logical expressions and short circuiting

--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -83,7 +83,7 @@ class Parser {
     }
 
     private Expr assignement() {
-	Expr expr = equality();
+	Expr expr = or();
 
 	if (match(EQUAL)) {
 	    Token equals = previous();
@@ -95,6 +95,30 @@ class Parser {
 	    }
 
 	    error(equals, "Invalid assignement target.");
+	}
+
+	return expr;
+    }
+
+    private Expr or() {
+	Expr expr = and();
+
+	while (match(OR)) {
+	    Token operator = previous();
+	    Expr right = and();
+	    expr = new Expr.Logical(expr, operator, right);
+	}
+
+	return expr;
+    }
+
+    private Expr and() {
+	Expr expr = equality();
+
+	while (match(AND)) {
+	    Token operator = previous();
+	    Expr right = equality();
+	    expr = new Expr.Logical(expr, operator, right);
 	}
 
 	return expr;
