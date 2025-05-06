af0ad5b4ffc291a4ce4a7f8eec6b3914fad3d27a
Christian Egli <christian.egli@sbs.ch>
2025-05-06 11:36:33 +0200

    Implement the while statement

--- src/main/java/com/craftinginterpreters/lox/Parser.java
+++ src/main/java/com/craftinginterpreters/lox/Parser.java
@@ -26,6 +26,7 @@ class Parser {
     private Stmt statement() {
 	if (match(IF)) return ifStatement();
 	if (match(PRINT)) return printStatement();
+	if (match(WHILE)) return whileStatement();
 	if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
 	return expressionStatement();
@@ -62,6 +63,15 @@ class Parser {
 
 	consume(SEMICOLON, "Expect ';' after variable declaration.");
 	return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt whileStatement() {
+	consume(LEFT_PAREN, "Expect '(' after 'while'.");
+	Expr condition = expression();
+	consume(RIGHT_PAREN, "Expect ')' after if condition.");
+	Stmt body = statement();
+
+	return new Stmt.While(condition, body);
     }
 
     private Stmt expressionStatement() {
